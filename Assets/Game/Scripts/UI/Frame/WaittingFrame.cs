22800932b46c89c4e5848b368818681760cce4ad
Takumi Sugimoto <ts20030211@gmail.com>
2024-08-15 10:28:21 +0900

    ui replacement

---
+++ Assets/Game/Scripts/UI/Frame/WaittingFrame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MatchingWaitFrame : MonoBehaviour
+{
+    [SerializeField]
+    Canvas _canvas;
+
+    public void ShowFrame()
+    {
+        _canvas.enabled = true;
+    }
+
+    public void HideFrame()
+    {
+        _canvas.enabled = false;
+    }
+
+}
